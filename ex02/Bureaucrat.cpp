1b338e4bde7964f6df0ef76e22d866d4edc07465
babayagaboy <hugo.guterres.2005@gmail.com>
2026-07-03 11:24:31 +0100

    ex01 done

---
+++ ex02/Bureaucrat.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgutterr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/10 19:28:27 by hgutterr          #+#    #+#             */
+/*   Updated: 2026/06/10 19:28:27 by hgutterr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat( void ) : _name("Default"), _grade(150) {
+	// std::cout << "Bureaucrat Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat( std::string name, int grade ) : _name(name)
+{
+	if (grade < 1)
+		throw (Bureaucrat::GradeTooHighException());
+	else if (grade > 150)
+		throw (Bureaucrat::GradeTooLowException());
+	else
+		this->_grade = grade;
+	// std::cout << "Bureaucrat Default constructor called" << std::endl;
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat& other )
+{
+	// std::cout << "Bureaucrat Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Bureaucrat& Bureaucrat::operator=( const Bureaucrat& other )
+{
+	// std::cout << "Bureaucrat Copy assignment called" << std::endl;
+	if (this != &other)
+	{
+		const_cast<std::string&>(this->_name) = other.getName();
+		this->_grade = other._grade;
+	}
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat( void ) {
+	// std::cout << "Bureaucrat Destructor called" << std::endl;
+}
+
+const std::string	Bureaucrat::getName() const {
+	return (this->_name);
+}
+
+int	Bureaucrat::getGrade() const {
+	return (this->_grade);
+}
+
+void	Bureaucrat::incrementGrade( void )
+{
+	if (this->_grade - 1 >= 1)
+		--this->_grade;
+	else
+		throw (Bureaucrat::GradeTooHighException());
+}
+
+void	Bureaucrat::decrementGrade( void )
+{
+	if (this->_grade + 1 <= 150)
+		++this->_grade;
+	else
+		throw (Bureaucrat::GradeTooLowException());
+}
+
+std::ostream &operator<<(std::ostream &o, const Bureaucrat& bureaucrat)
+{
+	o << bureaucrat.getName() << ", bureaucrat grade " << bureaucrat.getGrade() << ".";
+	return (o);
+}
---
+++ ex02/Bureaucrat.hpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hgutterr <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/10 19:28:26 by hgutterr          #+#    #+#             */
+/*   Updated: 2026/06/10 19:28:26 by hgutterr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <exception>
+
+class Bureaucrat {
+	private:
+		const std::string	_name;
+		int					_grade;
+	public:
+
+		class GradeTooHighException : public std::exception {
+			public:
+				virtual const char* what() const throw () {
+					return ("Grade too high!");
+				}
+		};
+	
+		class GradeTooLowException : public std::exception {
+			public:
+				virtual const char* what() const throw () {
+					return ("Grade too low!");
+				}
+		};
+
+		Bureaucrat( void );
+		Bureaucrat( std::string name, int grade );
+		Bureaucrat( const Bureaucrat& other );
+		Bureaucrat& operator=( const Bureaucrat& other );
+		~Bureaucrat( void );
+
+		const std::string	getName( void ) const;
+		int					getGrade( void ) const;
+		void				incrementGrade( void );
+		void				decrementGrade( void );
+};
+
+std::ostream& operator<<( std::ostream &o, const Bureaucrat &other );
+
+#endif
